3c5f42fe10c818cfd3e8fdffe952b91c2ef95af3
Oscar Loza <olcorona97@gmail.com>
2020-09-25 20:45:50 -0700

    character sets and the second algo

--- CharSets/charsets.js
+++ CharSets/charsets.js
@@ -1,11 +1,20 @@
 /* 
     [**] matches the internal of the square brakts
+    [1-6] or [a-z] or [1-6a-zA-z -] - these are ranges
+    ^ start of line or for negation like in [^]
 
+    Shorthands
+    \d - [0-9]
+    \w - [a-zA-z0-9]
+    \s - [\t\r\n]
+    \D - [^0-9]
+    \W - [^a-zA-Z0-9]
+    \S - [^\t\r\n]
 
 */
 
-let txt = "grey or gray";
+let txt = "“He is holding his hat in his hand.”";
 
-let regEx = /gr[ae]y/g;
+let regEx = /[ .a-z]/g;
 
 console.log(txt.match(regEx));
